4eb5223b860ede9fa354bd4c41a22c9998e85e98
Luai-Salah <77834783+Luai-Salah@users.noreply.github.com>
2022-07-07 03:21:28 +0200

    Fixes

--- GMTK GJ/Assets/Scripts/GameManager.cs
+++ GMTK GJ/Assets/Scripts/GameManager.cs
@@ -1,9 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : Singelton<GameManager>
 {
+    public GameObject go;
     public int value = 5;
 
     // Start is called before the first frame update
